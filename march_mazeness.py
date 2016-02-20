474bd555f3d8446270a88a5aa65ae9612345db3c
garbage-code <startrekrules4@gmail.com>
2016-02-20 14:10:43 -0500

    march_mazeness_noFog_key.py
    First working version. Movement working. Hit detection working.

--- march_mazeness.py
+++ march_mazeness.py
@@ -16,7 +16,7 @@ def play():
     global line8
     global line9
     global line10
-    line1 = ['p', '.', '.', '.', '■', 'i', '.', '.', '.', '■']
+    line1 = ['e', '.', '.', '.', '■', 'i', '.', '.', '.', '■']
     line2 = ['■', '■', '■', '.', '■', '■', '■', '■', '.', '.']
     line3 = ['■', '■', '■', '.', '■', '■', '■', '■', '■', '.']
     line4 = ['■', '■', '■', '.', '.', '.', '.', '.', '.', '.']
@@ -25,8 +25,9 @@ def play():
     line7 = ['■', '■', '■', '■', '▒', '.', '.', '.', '.', '■']
     line8 = ['■', '■', '■', '■', '.', '.', '■', '■', '■', '■']
     line9 = ['■', '■', '■', '■', '.', '.', '.', '.', '.', '.']
-    line10 = ['■', '■', '■', '■', '■', '■', '■', '■', '.', '@']
+    line10 = ['■', '■', '■', '■', '■', '■', '■', '■', '■', '@']
     hasKey = 0
+    coordinates = (1, 1)
     while line10[9] != "e":
         print(" ".join(line1))
         print(" ".join(line2))
@@ -41,152 +42,210 @@ def play():
         if line1[0] == "e":
             coordinates = 1, 1
             line1[0] = "."
-        if line1[1] == "e":
+        elif line1[1] == "e":
             coordinates = 2, 1
             line1[1] = "."
-        if line1[2] == "e":
+        elif line1[2] == "e":
             coordinates = 3, 1
             line1[2] = "."
-        if line1[3] == "e":
+        elif line1[3] == "e":
             coordinates = 4, 1
             line1[3] = "."
-        if line1[5] == "e":
+        elif line1[5] == "e":
             hasKey = 1            
             coordinates = 6, 1
             line1[5] = "."
-        if line1[6] == "e":
+        elif line1[6] == "e":
             coordinates = 7, 1
             line1[6] = "."
-        if line1[7] == "e":
+        elif line1[7] == "e":
             coordinates = 8, 1
             line1[7] = "."
-        if line1[8] == "e":
+        elif line1[8] == "e":
             coordinates = 9, 1
             line1[8] = "."
-        if line2[3] == "e":
+        elif line2[3] == "e":
             coordinates = 4, 2
             line2[3] = "."
-        if line2[8] == "e":
+        elif line2[8] == "e":
             coordinates = 9, 2
             line2[8] = "."
-        if line2[9] == "e":
+        elif line2[9] == "e":
             coordinates = 10, 2
             line2[9] = "."
-        if line3[3] == "e":
+        elif line3[3] == "e":
             coordinates = 4, 3
             line3[3] = "."
-        if line3[9] == "e":
+        elif line3[9] == "e":
             coordinates = 10, 3
             line3[9] = "."
-        if line4[3] == "e":
+        elif line4[3] == "e":
             coordinates = 4, 4
             line4[3] = "."
-        if line4[4] == "e":
+        elif line4[4] == "e":
             coordinates = 5, 4
             line4[4] = "."
-        if line4[5] == "e":
+        elif line4[5] == "e":
             coordinates = 6, 4
             line4[5] = "."
-        if line4[6] == "e":
+        elif line4[6] == "e":
             coordinates = 7, 4
             line4[6] = "."
-        if line4[7] == "e":
+        elif line4[7] == "e":
             coordinates = 8, 4
             line4[7] = "."
-        if line4[8] == "e":
+        elif line4[8] == "e":
             coordinates = 9, 4
             line4[8] = "."
-        if line4[9] == "e":
+        elif line4[9] == "e":
             coordinates = 10, 4
             line4[9] = "."
-        if line5[3] == "e":
+        elif line5[3] == "e":
             coordinates = 4, 5
             line5[3] = "."
-        if line6[3] == "e":
+        elif line6[3] == "e":
             coordinates = 4, 6
             line6[3] = "."
-        if line6[4] == "e":
+        elif line6[4] == "e":
             coordinates = 5, 6
             line6[4] = "."
-        if line6[8] == "e":
+        elif line6[8] == "e":
             coordinates = 9, 6
             line6[8] = "."
-        if line6[9] == "e":
+        elif line6[9] == "e":
             coordinates = 10, 6
             line6[9] = "."
-        if line7[4] == "e":
+        elif line7[4] == "e":
             coordinates = 5, 7
             if hasKey == 1:
                 line7[4] = "."
                 print("The door promptly falls off of its hinges and then disintegrates. You hope you don't meet the same fate.")
-        if line1[2] == "e":
-            coordinates = 3, 1
-            line1[2] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
-        if line1[2] == "e":
-            coordinates = 3, 1
-            line1[2] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
-        if line1[2] == "e":
-            coordinates = 3, 1
-            line1[2] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
-        if line1[2] == "e":
-            coordinates = 3, 1
-            line1[2] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
-        if line1[2] == "e":
-            coordinates = 3, 1
-            line1[2] = "."
-        if line1[0] == "e":
-            coordinates = 1, 1
-            line1[0] = "."
-        if line1[1] == "e":
-            coordinates = 2, 1
-            line1[1] = "."
+            elif hasKey == 0:
+                print("You need a key to open this oddly printed screen door!")
+        elif line7[5] == "e":
+            coordinates = 6, 7
+            line7[5] = "."
+        elif line7[6] == "e":
+            coordinates = 7, 7
+            line7[6] = "."
+        elif line7[7] == "e":
+            coordinates = 8, 7
+            line7[7] = "."
+        elif line7[8] == "e":
+            coordinates = 9, 7
+            line7[8] = "."
+        elif line8[4] == "e":
+            coordinates = 5, 8
+            line8[4] = "."
+        elif line9[4] == "e":
+            coordinates = 5, 9
+            line9[4] = "."
+        elif line9[5] == "e":
+            coordinates = 6, 9
+            line9[5] = "."
+        elif line9[6] == "e":
+            coordinates = 7, 9
+            line9[6] = "."
+        elif line9[7] == "e":
+            coordinates = 8, 9
+            line9[7] = "."
+        elif line9[8] == "e":
+            coordinates = 9, 9
+            line9[8] = "."
+        elif line9[9] == "e":
+            coordinates = 10, 9
+            line9[9] = "."
+        elif line10[9] == "e":
+            coordinates = 10, 10
+            line10[9] = "."
+            print("You won!")
+        control = input("> ")
+        if control == "w":
+            coordinates = coordinates[0], coordinates[1] - 1
+        elif control == "a":
+            coordinates = coordinates[0] - 1, coordinates[1]
+        elif control == "s":
+            coordinates = coordinates[0], coordinates[1] + 1
+        elif control == "d":
+            coordinates = coordinates[0] + 1, coordinates[1]
+        else:
+            print("invalid move.")
+        if coordinates == (1, 1):
+            line1[0] = "e"
+        elif coordinates == (2, 1):
+            line1[1] = "e"
+        elif coordinates == (3, 1):
+            line1[2] = "e"
+        elif coordinates == (4, 1):
+            line1[3] = "e"
+        elif coordinates == (6, 1):
+            line1[5] = "e"
+        elif coordinates == (7, 1):
+            line1[6] = "e"
+        elif coordinates == (8, 1):
+            line1[7] = "e"
+        elif coordinates == (9, 1):
+            line1[8] = "e"
+        elif coordinates == (4, 2):
+            line2[3] = "e"
+        elif coordinates == (9, 2):
+            line2[8] = "e"
+        elif coordinates == (10, 2):
+            line2[9] = "e"
+        elif coordinates == (4, 3):
+            line3[3] = "e"
+        elif coordinates == (10, 3):
+            line3[9] = "e"
+        elif coordinates == (4, 4):
+            line4[3] = "e"
+        elif coordinates == (5, 4):
+            line4[4] = "e"
+        elif coordinates == (6, 4):
+            line4[5] = "e"
+        elif coordinates == (7, 4):
+            line4[6] = "e"
+        elif coordinates == (8, 4):
+            line4[7] = "e"
+        elif coordinates == (9, 4):
+            line4[8] = "e"
+        elif coordinates == (10, 4):
+            line4[9] = "e"
+        elif coordinates == (4, 5):
+            line5[3] = "e"
+        elif coordinates == (4, 6):
+            line6[3] = "e"
+        elif coordinates == (5, 6):
+            line6[4] = "e"
+        elif coordinates == (9, 6):
+            line6[8] = "e"
+        elif coordinates == (10, 6):
+            line6[9] = "e"
+        elif coordinates == (5, 7):
+            line7[4] = "e"
+        elif coordinates == (6, 7):
+            line7[5] = "e"
+        elif coordinates == (7, 7):           
+            line7[6] = "e"
+        elif coordinates == (8, 7):           
+            line7[7] = "e"
+        elif coordinates == (9, 7):
+            line7[8] = "e"
+        elif coordinates == (5, 8):
+            line8[4] = "e"
+        elif coordinates == (5, 9):    
+            line9[4] = "e"
+        elif coordinates == (6, 9):    
+            line9[5] = "e"
+        elif coordinates == (7, 9):
+            line9[6] = "e"
+        elif coordinates == (8, 9): 
+            line9[7] = "e"
+        elif coordinates == (9, 9):           
+            line9[8] = "e"
+        elif coordinates == (10, 9):         
+            line9[9] = "e"
+        elif coordinates == (10, 10):       
+            line10[9] = "e"
+            print("You won!")
 
             
