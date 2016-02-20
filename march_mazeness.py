869872a00ff2d6e61757cbada8d65e7680727d9a
garbage-code <startrekrules4@gmail.com>
2016-02-20 16:14:09 -0500

    march_mazeness_noFog_yesKey_yesOOB_noWalls.py
    c'mon man

--- march_mazeness.py
+++ march_mazeness.py
@@ -23,7 +23,7 @@ def play():
     line5 = ['■', '■', '■', '.', '■', '■', '■', '■', '■', '■']
     line6 = ['■', '■', '■', '.', '.', '■', '■', '■', '.', '.']
     line7 = ['■', '■', '■', '■', '▒', '.', '.', '.', '.', '■']
-    line8 = ['■', '■', '■', '■', '.', '.', '■', '■', '■', '■']
+    line8 = ['■', '■', '■', '■', '.', '■', '■', '■', '■', '■']
     line9 = ['■', '■', '■', '■', '.', '.', '.', '.', '.', '.']
     line10 = ['■', '■', '■', '■', '■', '■', '■', '■', '■', '@']
     hasKey = 0
@@ -117,11 +117,7 @@ def play():
             line6[9] = "."
         elif line7[4] == "e":
             coordinates = 5, 7
-            if hasKey == 1:
-                line7[4] = "."
-                print("The door promptly falls off of its hinges and then disintegrates. You hope you don't meet the same fate.")
-            elif hasKey == 0:
-                print("You need a key to open this oddly printed screen door!")
+            line7[4] = "."
         elif line7[5] == "e":
             coordinates = 6, 7
             line7[5] = "."
@@ -159,8 +155,9 @@ def play():
             coordinates = 10, 10
             line10[9] = "."
             print("You won!")
-        control = input("> ")
-        if control == "w":
+        precontrol_coords = coordinates
+        control = input("> ")  #briefs for input
+        if control == "w":     #begin section b
             coordinates = coordinates[0], coordinates[1] - 1
         elif control == "a":
             coordinates = coordinates[0] - 1, coordinates[1]
@@ -169,7 +166,8 @@ def play():
         elif control == "d":
             coordinates = coordinates[0] + 1, coordinates[1]
         else:
-            print("invalid move.")
+            coordinates = coordinates
+            print("Please reconsider.")
         if coordinates == (1, 1):
             line1[0] = "e"
         elif coordinates == (2, 1):
@@ -180,6 +178,8 @@ def play():
             line1[3] = "e"
         elif coordinates == (6, 1):
             line1[5] = "e"
+            hasKey = 1
+            print("You found the oddly shaped key? Is this even a key?")
         elif coordinates == (7, 1):
             line1[6] = "e"
         elif coordinates == (8, 1):
@@ -221,7 +221,11 @@ def play():
         elif coordinates == (10, 6):
             line6[9] = "e"
         elif coordinates == (5, 7):
-            line7[4] = "e"
+            if hasKey == 1:
+                line7[4] = "e"
+                print("The door promptly falls off of its hinges and then disintegrates. You hope you don't meet the same fate.")
+            elif hasKey == 0:
+                print("You need a key to open this oddly printed screen door!")
         elif coordinates == (6, 7):
             line7[5] = "e"
         elif coordinates == (7, 7):           
@@ -246,6 +250,356 @@ def play():
             line9[9] = "e"
         elif coordinates == (10, 10):       
             line10[9] = "e"
-            print("You won!")
+            print("You won!")    
+        if coordinates[0] == 0:
+            print("Invalid move. In a perfect world, you'd have fallen into an impecabbly animated pit of lava around the maze. Alternatively, you've fallen outside of this 5 x 5 ascii set of lists.")
+            coordinates = precontrol_coords
+            if coordinates == (1, 1):
+                line1[0] = "e"
+            elif coordinates == (2, 1):
+                line1[1] = "e"
+            elif coordinates == (3, 1):
+                line1[2] = "e"
+            elif coordinates == (4, 1):
+                line1[3] = "e"
+            elif coordinates == (6, 1):
+                line1[5] = "e"
+                hasKey = 1
+                print("You found the oddly shaped key? Is this even a key?")
+            elif coordinates == (7, 1):
+                line1[6] = "e"
+            elif coordinates == (8, 1):
+                line1[7] = "e"
+            elif coordinates == (9, 1):
+                line1[8] = "e"
+            elif coordinates == (4, 2):
+                line2[3] = "e"
+            elif coordinates == (9, 2):
+                line2[8] = "e"
+            elif coordinates == (10, 2):
+                line2[9] = "e"
+            elif coordinates == (4, 3):
+                line3[3] = "e"
+            elif coordinates == (10, 3):
+                line3[9] = "e"
+            elif coordinates == (4, 4):
+                line4[3] = "e"
+            elif coordinates == (5, 4):
+                line4[4] = "e"
+            elif coordinates == (6, 4):
+                line4[5] = "e"
+            elif coordinates == (7, 4):
+                line4[6] = "e"
+            elif coordinates == (8, 4):
+                line4[7] = "e"
+            elif coordinates == (9, 4):
+                line4[8] = "e"
+            elif coordinates == (10, 4):
+                line4[9] = "e"
+            elif coordinates == (4, 5):
+                line5[3] = "e"
+            elif coordinates == (4, 6):
+                line6[3] = "e"
+            elif coordinates == (5, 6):
+                line6[4] = "e"
+            elif coordinates == (9, 6):
+                line6[8] = "e"
+            elif coordinates == (10, 6):
+                line6[9] = "e"
+            elif coordinates == (5, 7):
+                if hasKey == 1:
+                    line7[4] = "e"
+                    print("The door promptly falls off of its hinges and then disintegrates. You hope you don't meet the same fate.")
+                elif hasKey == 0:
+                    print("You need a key to open this oddly printed screen door!")
+            elif coordinates == (6, 7):
+                line7[5] = "e"
+            elif coordinates == (7, 7):           
+                line7[6] = "e"
+            elif coordinates == (8, 7):           
+                line7[7] = "e"
+            elif coordinates == (9, 7):
+                line7[8] = "e"
+            elif coordinates == (5, 8):
+                line8[4] = "e"
+            elif coordinates == (5, 9):    
+                line9[4] = "e"
+            elif coordinates == (6, 9):    
+                line9[5] = "e"
+            elif coordinates == (7, 9):
+                line9[6] = "e"
+            elif coordinates == (8, 9): 
+                line9[7] = "e"
+            elif coordinates == (9, 9):           
+                line9[8] = "e"
+            elif coordinates == (10, 9):         
+                line9[9] = "e"
+            elif coordinates == (10, 10):       
+                line10[9] = "e"
+                print("You won!")  
+        elif coordinates[1] == 0:
+            print("Invalid move. In a perfect world, you'd have fallen into an impecabbly animated pit of lava around the maze. Alternatively, you've fallen outside of this 5 x 5 ascii set of lists.")
+            coordinates = precontrol_coords
+            if coordinates == (1, 1):
+                line1[0] = "e"
+            elif coordinates == (2, 1):
+                line1[1] = "e"
+            elif coordinates == (3, 1):
+                line1[2] = "e"
+            elif coordinates == (4, 1):
+                line1[3] = "e"
+            elif coordinates == (6, 1):
+                line1[5] = "e"
+                hasKey = 1
+                print("You found the oddly shaped key? Is this even a key?")
+            elif coordinates == (7, 1):
+                line1[6] = "e"
+            elif coordinates == (8, 1):
+                line1[7] = "e"
+            elif coordinates == (9, 1):
+                line1[8] = "e"
+            elif coordinates == (4, 2):
+                line2[3] = "e"
+            elif coordinates == (9, 2):
+                line2[8] = "e"
+            elif coordinates == (10, 2):
+                line2[9] = "e"
+            elif coordinates == (4, 3):
+                line3[3] = "e"
+            elif coordinates == (10, 3):
+                line3[9] = "e"
+            elif coordinates == (4, 4):
+                line4[3] = "e"
+            elif coordinates == (5, 4):
+                line4[4] = "e"
+            elif coordinates == (6, 4):
+                line4[5] = "e"
+            elif coordinates == (7, 4):
+                line4[6] = "e"
+            elif coordinates == (8, 4):
+                line4[7] = "e"
+            elif coordinates == (9, 4):
+                line4[8] = "e"
+            elif coordinates == (10, 4):
+                line4[9] = "e"
+            elif coordinates == (4, 5):
+                line5[3] = "e"
+            elif coordinates == (4, 6):
+                line6[3] = "e"
+            elif coordinates == (5, 6):
+                line6[4] = "e"
+            elif coordinates == (9, 6):
+                line6[8] = "e"
+            elif coordinates == (10, 6):
+                line6[9] = "e"
+            elif coordinates == (5, 7):
+                if hasKey == 1:
+                    line7[4] = "e"
+                    print("The door promptly falls off of its hinges and then disintegrates. You hope you don't meet the same fate.")
+                elif hasKey == 0:
+                    print("You need a key to open this oddly printed screen door!")
+            elif coordinates == (6, 7):
+                line7[5] = "e"
+            elif coordinates == (7, 7):           
+                line7[6] = "e"
+            elif coordinates == (8, 7):           
+                line7[7] = "e"
+            elif coordinates == (9, 7):
+                line7[8] = "e"
+            elif coordinates == (5, 8):
+                line8[4] = "e"
+            elif coordinates == (5, 9):    
+                line9[4] = "e"
+            elif coordinates == (6, 9):    
+                line9[5] = "e"
+            elif coordinates == (7, 9):
+                line9[6] = "e"
+            elif coordinates == (8, 9): 
+                line9[7] = "e"
+            elif coordinates == (9, 9):           
+                line9[8] = "e"
+            elif coordinates == (10, 9):         
+                line9[9] = "e"
+            elif coordinates == (10, 10):       
+                line10[9] = "e"
+                print("You won!")  
+        elif coordinates[0] == 11:
+            print("Invalid move. In a perfect world, you'd have fallen into an impecabbly animated pit of lava around the maze. Alternatively, you've fallen outside of this 5 x 5 ascii set of lists.")
+            coordinates = precontrol_coords
+            if coordinates == (1, 1):
+                line1[0] = "e"
+            elif coordinates == (2, 1):
+                line1[1] = "e"
+            elif coordinates == (3, 1):
+                line1[2] = "e"
+            elif coordinates == (4, 1):
+                line1[3] = "e"
+            elif coordinates == (6, 1):
+                line1[5] = "e"
+                hasKey = 1
+                print("You found the oddly shaped key? Is this even a key?")
+            elif coordinates == (7, 1):
+                line1[6] = "e"
+            elif coordinates == (8, 1):
+                line1[7] = "e"
+            elif coordinates == (9, 1):
+                line1[8] = "e"
+            elif coordinates == (4, 2):
+                line2[3] = "e"
+            elif coordinates == (9, 2):
+                line2[8] = "e"
+            elif coordinates == (10, 2):
+                line2[9] = "e"
+            elif coordinates == (4, 3):
+                line3[3] = "e"
+            elif coordinates == (10, 3):
+                line3[9] = "e"
+            elif coordinates == (4, 4):
+                line4[3] = "e"
+            elif coordinates == (5, 4):
+                line4[4] = "e"
+            elif coordinates == (6, 4):
+                line4[5] = "e"
+            elif coordinates == (7, 4):
+                line4[6] = "e"
+            elif coordinates == (8, 4):
+                line4[7] = "e"
+            elif coordinates == (9, 4):
+                line4[8] = "e"
+            elif coordinates == (10, 4):
+                line4[9] = "e"
+            elif coordinates == (4, 5):
+                line5[3] = "e"
+            elif coordinates == (4, 6):
+                line6[3] = "e"
+            elif coordinates == (5, 6):
+                line6[4] = "e"
+            elif coordinates == (9, 6):
+                line6[8] = "e"
+            elif coordinates == (10, 6):
+                line6[9] = "e"
+            elif coordinates == (5, 7):
+                if hasKey == 1:
+                    line7[4] = "e"
+                    print("The door promptly falls off of its hinges and then disintegrates. You hope you don't meet the same fate.")
+                elif hasKey == 0:
+                    print("You need a key to open this oddly printed screen door!")
+            elif coordinates == (6, 7):
+                line7[5] = "e"
+            elif coordinates == (7, 7):           
+                line7[6] = "e"
+            elif coordinates == (8, 7):           
+                line7[7] = "e"
+            elif coordinates == (9, 7):
+                line7[8] = "e"
+            elif coordinates == (5, 8):
+                line8[4] = "e"
+            elif coordinates == (5, 9):    
+                line9[4] = "e"
+            elif coordinates == (6, 9):    
+                line9[5] = "e"
+            elif coordinates == (7, 9):
+                line9[6] = "e"
+            elif coordinates == (8, 9): 
+                line9[7] = "e"
+            elif coordinates == (9, 9):           
+                line9[8] = "e"
+            elif coordinates == (10, 9):         
+                line9[9] = "e"
+            elif coordinates == (10, 10):       
+                line10[9] = "e"
+                print("You won!")  
+        elif coordinates[1] == 11:
+            print("Invalid move. In a perfect world, you'd have fallen into an impecabbly animated pit of lava around the maze. Alternatively, you've fallen outside of this 5 x 5 ascii set of lists.")
+            coordinates = precontrol_coords
+            if coordinates == (1, 1):
+                line1[0] = "e"
+            elif coordinates == (2, 1):
+                line1[1] = "e"
+            elif coordinates == (3, 1):
+                line1[2] = "e"
+            elif coordinates == (4, 1):
+                line1[3] = "e"
+            elif coordinates == (6, 1):
+                line1[5] = "e"
+                hasKey = 1
+                print("You found the oddly shaped key? Is this even a key?")
+            elif coordinates == (7, 1):
+                line1[6] = "e"
+            elif coordinates == (8, 1):
+                line1[7] = "e"
+            elif coordinates == (9, 1):
+                line1[8] = "e"
+            elif coordinates == (4, 2):
+                line2[3] = "e"
+            elif coordinates == (9, 2):
+                line2[8] = "e"
+            elif coordinates == (10, 2):
+                line2[9] = "e"
+            elif coordinates == (4, 3):
+                line3[3] = "e"
+            elif coordinates == (10, 3):
+                line3[9] = "e"
+            elif coordinates == (4, 4):
+                line4[3] = "e"
+            elif coordinates == (5, 4):
+                line4[4] = "e"
+            elif coordinates == (6, 4):
+                line4[5] = "e"
+            elif coordinates == (7, 4):
+                line4[6] = "e"
+            elif coordinates == (8, 4):
+                line4[7] = "e"
+            elif coordinates == (9, 4):
+                line4[8] = "e"
+            elif coordinates == (10, 4):
+                line4[9] = "e"
+            elif coordinates == (4, 5):
+                line5[3] = "e"
+            elif coordinates == (4, 6):
+                line6[3] = "e"
+            elif coordinates == (5, 6):
+                line6[4] = "e"
+            elif coordinates == (9, 6):
+                line6[8] = "e"
+            elif coordinates == (10, 6):
+                line6[9] = "e"
+            elif coordinates == (5, 7):
+                if hasKey == 1:
+                    line7[4] = "e"
+                    print("The door promptly falls off of its hinges and then disintegrates. You hope you don't meet the same fate.")
+                elif hasKey == 0:
+                    print("You need a key to open this oddly printed screen door!")
+            elif coordinates == (6, 7):
+                line7[5] = "e"
+            elif coordinates == (7, 7):           
+                line7[6] = "e"
+            elif coordinates == (8, 7):           
+                line7[7] = "e"
+            elif coordinates == (9, 7):
+                line7[8] = "e"
+            elif coordinates == (5, 8):
+                line8[4] = "e"
+            elif coordinates == (5, 9):    
+                line9[4] = "e"
+            elif coordinates == (6, 9):    
+                line9[5] = "e"
+            elif coordinates == (7, 9):
+                line9[6] = "e"
+            elif coordinates == (8, 9): 
+                line9[7] = "e"
+            elif coordinates == (9, 9):           
+                line9[8] = "e"
+            elif coordinates == (10, 9):         
+                line9[9] = "e"
+            elif coordinates == (10, 10):       
+                line10[9] = "e"
+                print("You won!")  
+        else:
+            print(" ".join(line1))
+            print(" ".join(line2))
+            print(" ".join(line3))   
+            print(" ".join(line4))  
+            print(" ".join(line5))
 
             
